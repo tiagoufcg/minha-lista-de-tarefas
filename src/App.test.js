ce9e3c59e515988f44d5af19f6a420166c1c69aa
Tiago Sales dos Santos <140715693+tiagoufcg@users.noreply.github.com>
2026-02-24 14:54:20 -0300

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderiza título da aplicação', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titulo = screen.getByText(/Minha Lista de Tarefas/i);
+  expect(titulo).toBeInTheDocument();
 });
